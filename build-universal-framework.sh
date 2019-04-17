39bfd3cb04d15d33b8efb5b66f064d24f52578c1
Pearson Henri <pearsonhenri@gmail.com>
2019-04-17 11:41:36 -0300

    change deployment target version

---
+++ build-universal-framework.sh
@@ -0,0 +1,38 @@
+rm -rf build && mkdir build
+
+# build framework for simulators
+xcodebuild clean build -workspace InspetorSwiftFramework.xcworkspace -scheme InspetorSwiftFramework -configuration Release -sdk iphonesimulator -derivedDataPath derived_data
+
+# create folder to store compiled framework for simulator
+mkdir build/simulator
+
+# copy compiled framework for simulator into our build folder
+cp -r derived_data/Build/Products/Release-iphonesimulator/InspetorSwiftFramework.framework build/simulator
+
+# build framework for devices
+xcodebuild clean build -workspace InspetorSwiftFramework.xcworkspace -scheme InspetorSwiftFramework -configuration Release -sdk iphoneos -derivedDataPath derived_data
+
+# create folder to store compiled framework for simulator
+mkdir build/devices
+
+# copy compiled framework for simulator into our build folder
+cp -r derived_data/Build/Products/Release-iphoneos/InspetorSwiftFramework.framework build/devices
+
+
+####################### Create universal framework #############################
+# create folder to store compiled universal framework
+mkdir build/universal
+
+# copy device framework into universal folder
+cp -r build/devices/InspetorSwiftFramework.framework build/universal/
+
+# create framework binary compatible with simulators and devices and replace binary in unviersal framework
+lipo -create build/simulator/InspetorSwiftFramework.framework/InspetorSwiftFramework build/devices/InspetorSwiftFramework.framework/InspetorSwiftFramework -output build/universal/InspetorSwiftFramework.framework/InspetorSwiftFramework
+
+# copy simulator Swift public interface to universal framework
+cp build/simulator/InspetorSwiftFramework.framework/Modules/InspetorSwiftFramework.swiftmodule/* build/universal/InspetorSwiftFramework.framework/Modules/InspetorSwiftFramework.swiftmodule
+
+# move binary to distribution repo (must be cloned alongside this repository)
+rm -rf ../inspetor-swift-framework-dist/backup/*
+mv ../inspetor-swift-framework-dist/InspetorSwiftFramework.framework ../inspetor-swift-framework-dist/backup
+mv build/universal/InspetorSwiftFramework.framework ../inspetor-swift-framework-dist
